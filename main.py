d6e0203184ed145f2899bb32d5d7ae0b6da1a155
pythondev <pythondev@ongraph.com>
2026-07-06 18:22:13 +0530

    Changes

--- main.py
+++ main.py
@@ -171,4 +171,7 @@ else:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    # Render requires binding to 0.0.0.0
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
